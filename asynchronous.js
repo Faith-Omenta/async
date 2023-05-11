fb35053d008067a2dd6efd89be2d01768426ba38
Faith-Omenta <faithbomenta@gmail.com>
2023-05-11 14:22:04 +0300

    async

--- asynchronous.js
+++ asynchronous.js
@@ -1,7 +1,10 @@
 // Write an asynchronous function that accepts a message string and a delay time 
 // in milliseconds. The function should log the message to the console after the 
 // specified delay time.
-
+function greeting=()=>{
+   console.log("I like rain")
+}
+setTimeout(greeting,2000);
 
 // You have an array of user IDs and a function getUserData(id) that returns a 
 // Promise with user data when given a user ID. Write an asynchronous function 
